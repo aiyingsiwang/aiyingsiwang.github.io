fe538ca19f1b9ffcf1c9578bbf758867d1cdef72
Liu Zixin <3620276154@qq.com>
2026-09-08 19:43:56 +0800

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -7,22 +7,25 @@ window.SNIPPETS = [
 
         keywords: [
             "powershell",
-            "java",
+            "java"
         ],
 
-        code: '$output = "output.txt"
-"===== 目录树 =====" | Out-File $output -Encoding UTF8
-tree /f | Out-File $output -Encoding UTF8 -Append
-
-"`n===== 文件内容 =====" | Out-File $output -Encoding UTF8 -Append
-
-Get-ChildItem -Recurse -File -Include *.java,*.xml | ForEach-Object {
-    "`n" + "="*50 | Out-File $output -Encoding UTF8 -Append
-    "文件路径: $($_.FullName)" | Out-File $output -Encoding UTF8 -Append
-    "="*50 | Out-File $output -Encoding UTF8 -Append
-    Get-Content $_.FullName -Encoding UTF8 -ErrorAction SilentlyContinue | Out-File $output -Encoding UTF8 -Append
-}
-
-Write-Host "完成！已生成 $output"'
+        code: [
+            '$output = "output.txt"',
+            '"===== 目录树 =====" | Out-File $output -Encoding UTF8',
+            'tree /f | Out-File $output -Encoding UTF8 -Append',
+            '',
+            '"`n===== 文件内容 =====" | Out-File $output -Encoding UTF8 -Append',
+            '',
+            'Get-ChildItem -Recurse -File -Include *.java,*.xml | ForEach-Object {',
+            '    "`n" + "="*50 | Out-File $output -Encoding UTF8 -Append',
+            '    "文件路径: $($_.FullName)" | Out-File $output -Encoding UTF8 -Append',
+            '    "="*50 | Out-File $output -Encoding UTF8 -Append',
+            '    Get-Content $_.FullName -Encoding UTF8 -ErrorAction SilentlyContinue | Out-File $output -Encoding UTF8 -Append',
+            '}',
+            '',
+            'Write-Host "完成！已生成 $output"'
+        ].join("\n")
     }
+
 ];
